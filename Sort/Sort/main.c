4f2b0c8b969231a79cbf3603c9120cc81e033533
Bharadwaj <bharadwajdaya@gmail.com>
2015-12-23 19:04:53 +0530

    Binary sort
    for

--- Sort/Sort/main.c
+++ Sort/Sort/main.c
@@ -7,9 +7,35 @@
 //
 
 #include <stdio.h>
+int binarysearch(int *,int ,int ,int );
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    printf("Hello, World!\n");
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int x=4;
+    int result=binarysearch(arr,0,n-1,x);
+    printf("index %d \n",result);
     return 0;
+}
+int binarysearch(int arr[],int l,int r,int x)
+{
+    if(r>=l)
+    {
+        int mid=l+(r-l)/2;
+        if(arr[mid]==x)
+        {
+            return mid;
+        }
+        if(arr[mid]>x)
+        {
+            return binarysearch(arr, l, mid-1, x);
+        }
+        if(arr[mid]<x)
+        {
+            return binarysearch(arr, mid+1, r, x);
+        }
+    }
+    return -1;
 }
