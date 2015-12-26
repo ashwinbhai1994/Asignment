628a29e37d7c23e264a348741dd78e1681833923
Bharadwaj <bharadwajdaya@gmail.com>
2015-12-26 20:08:58 +0530

    2dDynamic

--- SingularMatTran/SingularMatTran/main.c
+++ SingularMatTran/SingularMatTran/main.c
@@ -6,20 +6,26 @@
 //
 
 #include <stdio.h>
-void printarray(int n,int a[][n]);
+#include<stdlib.h>
+void printarray(int n,int **);
 int main(int argc, const char * argv[])
 {
     int row,col;
     int temp=0;
     int n=4;
     scanf("%d",&n);
-    
-    int a[4][4]={{1,2,3,4},
-                {4,5,6,4},
-                {7,8,9,4},
-                {10,11,12,13}
-                };
-    
+    int *a[n];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=(int *)malloc(n*sizeof(int));
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
     for(row=1;row<n;row++)
     {
         for(col=0;col<n-1;col++)
@@ -37,7 +43,7 @@ int main(int argc, const char * argv[])
     
     return 0;
 }
-void printarray(int n,int a[][n])
+void printarray(int n,int *a[])
 {
     for(int i=0;i<n;i++)
     {
