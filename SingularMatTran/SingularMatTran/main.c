a898d5e0c15df7927047d0c2a973a4a9757a99c7
Bharadwaj <bharadwajdaya@gmail.com>
2015-12-26 20:00:02 +0530

    1.0.2

--- SingularMatTran/SingularMatTran/main.c
+++ SingularMatTran/SingularMatTran/main.c
@@ -6,16 +6,20 @@
 //
 
 #include <stdio.h>
-
+void printarray(int n,int a[][n]);
 int main(int argc, const char * argv[])
 {
     int row,col;
     int temp=0;
-    int n=3;
-    int a[3][3]={{1,2,3},
-                {4,5,6},
-                {7,8,9}
+    int n=4;
+    scanf("%d",&n);
+    
+    int a[4][4]={{1,2,3,4},
+                {4,5,6,4},
+                {7,8,9,4},
+                {10,11,12,13}
                 };
+    
     for(row=1;row<n;row++)
     {
         for(col=0;col<n-1;col++)
@@ -28,6 +32,13 @@ int main(int argc, const char * argv[])
             }
         }
     }
+    printarray(n,a);
+    
+    
+    return 0;
+}
+void printarray(int n,int a[][n])
+{
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
@@ -36,7 +47,4 @@ int main(int argc, const char * argv[])
         }
         printf("\n");
     }
-    
-    
-    return 0;
 }
