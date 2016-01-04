67bbe10862ee7d993a1b797dc561110496b89cb9
Bharadwaj <bharadwajdaya@gmail.com>
2016-01-04 22:32:04 +0530

    patternoddeven

--- PatternOddEven/PatternOddEven/main.c
+++ PatternOddEven/PatternOddEven/main.c
@@ -13,26 +13,21 @@ int main(int argc, const char * argv[]) {
     int odd=1;
     int even=2;
     //int temp[n][n];
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i=i+2)
     {
         for(j=1;j<=i;j++)
         {
-            if(i%2!=0)
-            {
-                //temp[i][j]=odd;
-                printf("%d",odd);
-                odd+=2;
-                
-            }
-            else
-            {
-                //temp[i][j]=even;
-                printf("%d",even);
-                even+=2;
-            }
-            
+            printf("%d",odd);
+            odd+=2;
         }
         printf("\n");
+        for(j=1;j<=(i+1);j++)
+        {
+            printf("%d",even);
+            even+=2;
+        }
+        printf("\n");
+        
     }
     
     return 0;
