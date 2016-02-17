1c6cd87138c243661027266e63e37c4c24be5de5
Bharadwaj <bharadwajdaya@gmail.com>
2016-02-17 16:24:56 +0530

    nextPALINDROME.c

--- nextPalindrome.c
+++ nextPalindrome.c
@@ -1,213 +1,186 @@
-#include<stdio.h>
-char str[1000005];
+//
+//  main.c
+//  nextPalindrome.c
+//
+//  Created by Bharadwaj on 17/02/16.
+//  Copyright © 2016 Bharadwaj. All rights reserved.
+//
 
-int main()
-{
-    int t,i,j;
-    
-    scanf("%i",&t);
-    
-    while(t--) {
-        
-        scanf("%s",str);
-        
-        int lenght = strlen(str);
-        
-        j = lenght;
-        i = -1;
-        
-        while(++i <= --j) {
-            if(str[i] != str[j]) {
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void even(int *i,int *j,int count);
+void odd(int *i,int *j,int count);
+void aboveNine(char *,int ,int ,int);
+void Palindrome(char num[],int );
+int main(int argc, const char * argv[]) {
+    int i,j,count;
+    char num[10000]={0};
+    int f=0;
+    scanf("%s",num);
+    count=strlen(num);
+    j=count;
+    i=-1;
+    while(++i<=--j)
+    {
+        if(num[i]!=num[j])
+        {
+            break;
+        }
+    }
+    if(j<i)
+    {
+        Palindrome(num,count);
+    }
+    else
+    {
+        if(count%2!=0)
+        {
+            odd(&i, &j, count);
+        }
+        else
+        {
+            even(&i, &j, count);
+        }
+        while(i>=0)
+        {
+            if(num[i]-num[j]>0)
+            {
+                f=1;
                 break;
             }
+            else if(num[i]-num[j]<0)
+            {
+                f=0;
+                break;
+            }
+            i--;
+            j++;
         }
-        
-        if(j < i) {
-            /*   Number is palindrome   */
-            
-            if(lenght & 1) {
-                /* odd lenght  */
-                
-                if(str[lenght/2] < '9'){
-                    /* check the middle one not 9 */
-                    
-                    str[lenght/2]++;
-                    
-                    printf("%s\n",str);
-                }
-                
-                else {
-                    str[lenght/2] = '0';
-                    
-                    i = lenght/2 - 1;
-                    j = lenght/2 + 1;
-                    
-                    while(i >= 0) {
-                        
-                        if(str[i] < '9') {
-                            str[i]++;
-                            str[j]++;
-                            break;
-                        }
-                        
-                        else {
-                            str[i] = str[j] = '0';
-                        }
-                        
-                        i--;
-                        j++;
-                    }
-                    
-                    if(i < 0) {
-                        printf("1");
-                        
-                        i = lenght;
-                        while(--i > 0) {
-                            printf("0");
-                        }
-                        
-                        printf("1\n");
-                    }
-                    
-                    else {
-                        
-                        printf("%s\n",str);
-                    }
-                }
-            }
-            
-            else {
-                /*  even lenght  */
-                
-                i = lenght/2 - 1;
-                j = lenght/2;
-                
-                while(i >= 0) {
-                    
-                    if(str[i] < '9') {
-                        str[i]++;
-                        str[j]++;
-                        break;
-                    }
-                    
-                    else {
-                        str[i] = str[j] = '0';
-                    }
-                    
-                    i--;
-                    j++;
-                }
-                
-                if(i < 0) {
-                    printf("1");
-                    
-                    i = lenght;
-                    while(--i > 0) {
-                        printf("0");
-                    }
-                    
-                    printf("1\n");
-                }
-                
-                else {
-                    
-                    printf("%s\n",str);
-                }
-            }
+        if(count%2 !=0)
+        {
+            odd(&i, &j, count);
         }
-        
-        else {
-            if(lenght & 1) {
-                i = lenght/2 - 1;
-                j = lenght/2 + 1;
-            }
-            
-            else {
-                i =lenght/2 - 1;
-                j = lenght/2;
-            }
-            
-            int flag;
-            
-            while(i >= 0) {
-                
-                if(str[i] - str[j] > 0) {
-                    flag = 1;
-                    break;
-                }
-                
-                else if( str[i] - str[j] < 0 ) {
-                    flag = 2;
-                    break;
-                }
-                
+        else
+        {
+            even(&i, &j, count);
+        }
+        if(f==1)
+        {
+            while(i>=0)
+            {
+                num[j]=num[i];
                 i--;
                 j++;
             }
+        }
+        else if(f==0&&num[count/2]<'9'&&count%2!=0)
+        {
             
-            if(lenght & 1) {
-                i = lenght/2 - 1;
-                j = lenght/2 + 1;
+            num[count/2]++;
+            odd(&i, &j, count);
+            while(i>=0)
+            {
+                num[j]=num[i];
+                i--;
+                j++;
             }
-            
-            else {
-                i = lenght/2 - 1;
-                j = lenght/2;
+        }
+        else
+        {
+            if(count%2!=0)
+            {
+                num[count/2]='0';
             }
-            
-            if(flag == 1) {    /*  line 1*/
-                while(i >= 0) {
-                    str[j] = str[i];
-                    
-                    i--;
-                    j++;
+            while(i>=0)
+            {
+                if(num[i]<'9')
+                {
+                    num[i]++;
+                    num[j]=num[i];
+                    break;
                 }
+                else
+                {
+                    num[i]='0';
+                    num[j]='0';
+                }
+                i--;
+                j++;
             }
-            
-            else if(flag == 2 && lenght&1 && str[lenght/2] < '9'){    /* line 2*/
-                str[lenght/2]++;
-                
-                i = lenght/2 - 1;
-                j = lenght/2 + 1;
-                
-                while(i >= 0) {
-                    str[j] = str[i];
-                    i--;
-                    j++;
-                }
+            while(i>=0)
+            {
+                num[j]=num[i];
+                i--;
+                j--;
             }
-            
-            else {                          /* line 3   */
-                
-                if( lenght & 1) {
-                    str[lenght/2] = '0';
-                }
-                
-                while(i >= 0) {
-                    if(str[i] < '9') {
-                        str[i]++;
-                        str[j] = str[i];
-                        break;
-                    }
-                    
-                    else {
-                        str[i] = str[j] = '0';
-                    }
-                    i--;
-                    j++;
-                }
-                
-                while(i >= 0) {
-                    str[j] = str[i];
-                    
-                    i--;
-                    j++;
-                }
-            }
-            
-            printf("%s\n",str);
+        }
+        printf("%s",num);
+    }
+    return 0;
+}
+void Palindrome(char num[],int count)
+{
+    int i,j;
+    if(count%2!=0)
+    {
+        if(num[count/2]<'9')
+        {
+            num[count/2]++;
+            printf("%s",num);
+        }
+        else
+        {
+            num[count/2]='0';
+            odd(&i,&j,count);
+            aboveNine(num,count,i,j);
         }
     }
+    else
+    {
+        even( &i, &j, count);
+        aboveNine(num, count, i, j);
+    }
+}
+void odd(int *i,int *j,int count)
+{
+    *i=(count/2) - 1;
+    *j=(count/2)+1;
     
-    return 0;
+}
+void even(int *i,int *j,int count)
+{
+    *i=count/2 -1;
+    *j=count/2;
+}
+void aboveNine(char num[],int count,int i,int j)
+{
+    while(i>=0)
+    {
+        if(num[i]<'9')
+        {
+            num[i]++;
+            num[j]++;
+            break;
+        }
+        else
+        {
+            num[i]=num[j]='0';
+        }
+        i--;
+        j++;
+    }
+    if(i<0)
+    {
+        printf("1");
+        for(int k=1;k<count;k++)
+        {
+            printf("0");
+        }
+        printf("1");
+    }
+    else
+    {
+        printf("%s",num);
+    }
 }
