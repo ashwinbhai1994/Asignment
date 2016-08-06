8505bc401fce25fe2b987bb2c47f58b351889966
Bharadwaj <bharadwajdaya@gmail.com>
2016-08-06 22:01:04 +0530

    Update reduce.c
    reduce to fraction

--- reduce.c
+++ reduce.c
@@ -1,27 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int findGcd(int a,int b);
-int main()
-{
-	char str[100]="9/12";
-	printf("Enter the fraction\n");
-	char *token=strtok(str,"/");
-	int num=atoi(token);
-	token=strtok(NULL,"/");
-	int den=atoi(token);
-	int gcd=findGcd(num,den);
-	printf("%d/%d",(num/gcd),(den/gcd));
-	return 0;
+//Reduce to fraction
+
+char** ReduceFraction(int n, char** str, int* res) {
+    int i=0;
+    int num=0;
+    char **ans=(char**)malloc(sizeof(char*)*n);
+    for(i=0;i<n;i++)
+    {
+            ans[i]=(char*)malloc(sizeof(char)*10);
+    }
+    int den=0;
+    char *token;
+    int gcd=0;
+    for(i=0;i<n;i++)
+    {
+         token=strtok(str[i],"/");
+         num=atoi(token);
+        token=strtok(NULL,"/");
+        den=atoi(token);
+        gcd=findGcd(num,den);
+        sprintf(ans[i],"%d/%d",num/gcd,den/gcd);
+        printf("%s",ans[i]);
+    }
+    *res=n;
+    return ans;
 }
 int findGcd(int a,int b)
 {
-	if(b!=0)
-	{
-		return findGcd(b,a%b);
-	}
-	else
-	{
-		return a;
-	}
+    if(b!=0)
+        {
+        return findGcd(b,a%b);
+    }
+    else
+        {
+        return a;
+    }
 }
